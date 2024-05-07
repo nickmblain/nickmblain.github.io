d30f2345d26037bd5ec0bf86c3cce79d85ed9690
Nick Blain <nblain@cmnhospitals.org>
2024-05-07 16:42:25 -0600

    Increase the CMN logo size

--- src/components/experience/experience.vue
+++ src/components/experience/experience.vue
@@ -15,7 +15,7 @@
                         <div class="ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-amber-500 md:after:mx-auto ltr:md:after:right-0 ltr:md:after:left-0 rtl:md:after:left-0 rtl:md:after:right-0 ltr:after:-left-9 rtl:after:-right-9 before:content-[''] before:absolute md:before:mx-auto ltr:md:before:right-0 ltr:md:before:left-0 ltr:before:-left-11 rtl:md:before:left-0 rtl:md:before:right-0 rtl:before:-right-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
                             <div class="grid md:grid-cols-2">
                                 <div class="md:text-end md:me-8 relative wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
-                                    <img src="https://resources.childrensmiraclenetworkhospitals.org/cdn/images/cmnh-logos/CMNH_Logo_Primary_RGB.svg" class="rounded-full h-9 w-9 md:ms-auto" alt="">
+                                    <img src="https://resources.childrensmiraclenetworkhospitals.org/cdn/images/cmnh-logos/CMNH_Logo_Primary_RGB.svg" class="h-9 md:ms-auto" alt="">
                                     <h5 class="my-2 font-semibold text-lg">Children's Miracle Network Hospitals</h5>
                                     <h6 class="text-sm mb-0">01/2022 to Present</h6>
                                 </div>
@@ -32,7 +32,7 @@
                         <div class="mt-12 ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-amber-500 md:after:mx-auto ltr:md:after:right-0 ltr:md:after:left-0 ltr:after:-left-9 rtl:md:after:left-0 rtl:md:after:right-0 rtl:after:-right-9 before:content-[''] before:absolute md:before:mx-auto ltr:md:before:right-0 ltr:md:before:left-0 ltr:before:-left-11 rtl:md:before:left-0 rtl:md:before:right-0 rtl:before:-right-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
                             <div class="grid md:grid-cols-2">
                                 <div class="text-start md:ms-8 relative md:order-2 wow animate__animated animate__fadeInUp" data-wow-delay=".7s">
-                                    <img src="https://resources.childrensmiraclenetworkhospitals.org/cdn/images/cmnh-logos/CMNH_Logo_Primary_RGB.svg" class="rounded-full h-9 w-9 md:me-auto" alt="">
+                                    <img src="https://resources.childrensmiraclenetworkhospitals.org/cdn/images/cmnh-logos/CMNH_Logo_Primary_RGB.svg" class="h-9 md:me-auto" alt="">
                                     <h5 class="my-2 font-semibold text-lg">Children's Miracle Network Hospitals</h5>
                                     <h6 class="text-sm mb-0">1/2016 to 12/2021</h6>
                                 </div>
@@ -49,7 +49,7 @@
                         <div class="mt-12 ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-amber-500 md:after:mx-auto ltr:md:after:right-0 ltr:md:after:left-0 ltr:after:-left-9 rtl:md:after:left-0 rtl:md:after:right-0 rtl:after:-right-9 before:content-[''] before:absolute md:before:mx-auto ltr:md:before:right-0 ltr:md:before:left-0 ltr:before:-left-11 rtl:md:before:left-0 rtl:md:before:right-0 rtl:before:-right-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
                             <div class="grid md:grid-cols-2">
                                 <div class="md:text-end md:me-8 relative wow animate__animated animate__fadeInUp" data-wow-delay="1.1s">
-                                    <img src="https://resources.childrensmiraclenetworkhospitals.org/cdn/images/cmnh-logos/CMNH_Logo_Primary_RGB.svg" class="rounded-full h-9 w-9 md:ms-auto" alt="">
+                                    <img src="https://resources.childrensmiraclenetworkhospitals.org/cdn/images/cmnh-logos/CMNH_Logo_Primary_RGB.svg" class="h-9 md:ms-auto" alt="">
                                     <h5 class="my-2 font-semibold text-lg">Children's Miracle Network Hospitals</h5>
                                     <h6 class="text-sm mb-0">12/2012 to 12/2015</h6>
                                 </div>
